1e5421a722fb81e167852a2e0d59a4e895481be4
pooreyoutuber <leonardo41111s@gmail.com>
2025-10-03 11:52:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY_HERE"; // replace with your YouTube Data API v3 key
+const API_KEY = "AIzaSyAI7S6lmMnAwDn0jTje_d1w_Qd9gQH_fAo"; // replace with your YouTube Data API v3 key
 const fetchBtn = document.getElementById("fetchBtn");
 const status = document.getElementById("status");
 
