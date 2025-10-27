7bc98ff1e9ce120ff9bf0739680c9342d0f93060
pooreyoutuber <leonardo41111s@gmail.com>
2025-10-27 16:13:17 +0530

    Update proxy_loader.php

--- proxy_loader.php
+++ proxy_loader.php
@@ -17,7 +17,7 @@ $ch = curl_init();
 $proxy_address = "$proxy_ip:$proxy_port";
 
 $headers = array(
-    // Use DESKTOP User Agent so the iframe displays the desktop version
+    // Use DESKTOP User Agent
     "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Language: en-US,en;q=0.9"
