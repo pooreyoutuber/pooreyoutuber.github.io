ade0452ca08bff4af0dbba9059ab08d7459220c7
pooreyoutuber <leonardo41111s@gmail.com>
2025-10-31 12:21:07 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 document.getElementById('launchBtn').addEventListener('click', () => {
+    // Input fields se data lena
     const userUrl = document.getElementById('userUrl').value.trim();
     const autoScroll = document.getElementById('autoScroll').checked;
     const autoRefresh = document.getElementById('autoRefresh').checked;
     
+    // Validation
     if (!userUrl) {
         alert("❌ Kripya Target URL daalein!");
         return;
