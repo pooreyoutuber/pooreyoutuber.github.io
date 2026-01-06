b9daeb66ee055561e3dbeeca109b6287bd4a799d
pooreyoutuber <leonardo41111s@gmail.com>
2026-01-06 20:31:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -931,7 +931,7 @@ app.post('/start-task', async (req, res) => {
                 const randomUrl = urls[Math.floor(Math.random() * urls.length)];
                 
                 console.log(`[QUEUE] View #${i} | Active URL: ${randomUrl}`);
-                await runstandGscTask(keyword, randomUrl, i); 
+                await runsGscTask(keyword, randomUrl, i); 
 
                 if (i < totalViews) {
                     // RAM management break
