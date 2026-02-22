d06af73f6a3bd7e372b91a1418e9c35df5f08d74
pooreyoutuber <leonardo41111s@gmail.com>
2026-02-22 13:23:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -823,7 +823,6 @@ app.get('/proxy-request', async (req, res) => {
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const puppeteer = require('puppeteer');
 const USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
