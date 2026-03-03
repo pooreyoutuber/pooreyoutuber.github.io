01c487771547e6e2110a5059fc24c05a7555d1b9
pooreyoutuber <leonardo41111s@gmail.com>
2026-03-03 17:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const express = require('express');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-
-
-const genAI = new GoogleGenerativeAI(GEMINI_KEY);
-
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash"
 });
