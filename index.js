f3098e17c1955d16fe83beffa8660fd9d68a2835
pooreyoutuber <leonardo41111s@gmail.com>
2026-03-21 17:40:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ================================================================
 // index.js (ULTIMATE FINAL VERSION - Part 1/2)
 // =============================================================
-
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 // --- Imports (Node.js Modules) ---
 const express = require('express');
 const nodeFetch = require('node-fetch'); 
